210a7a00172eecc31919450cb43edd9f3ddb778c
KyleBurton <kyleb286@gmail.com>
2018-12-11 13:07:21 -0600

    Update

---
+++ ArrayDemo/ArrayDemo/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace ArrayDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[8];
+            int i;
+
+            for (i = 0; i < 8; i++)
+            {
+                Write("Enter number - " + i + "<< ");
+                arr[i] = Convert.ToInt32(ReadLine());
+            }
+            int option;
+            WriteLine("Please choose option 1 for viewing the list in order from first to last," +
+                " or Choose option 2 to view in order from last to first, or Choose option 3 to select a position.");
+            option = Convert.ToInt32(ReadLine());
+            if (option == 1)
+            {
+                for (int j = 0; j < arr.Length; j++)
+                {
+                    Write(arr[j]);
+                }
+            }
+            else if (option == 2)
+            {
+                for (int b = 7; b >= 0; b--)
+                {
+                    WriteLine("Numbers entered are " + arr[b]);
+                }
+            }
+            else if (option == 3)
+            {
+                int choice;
+                WriteLine("Please pick a specific position you want to view");
+                choice = Convert.ToInt32(ReadLine());
+                WriteLine("The number you choose is" + arr[choice]);
+
+            }
+            ReadLine();
+        }
+    }
+}
---
+++ TemperatureList/TemperatureList/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace TemperatureList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] temp = new int[7];
+            int i;
+            int TempAverage;
+            int total = 0;
+
+            for (i = 0; i <temp.Length; i++)
+            {
+                WriteLine("Please enter the High tempertaures for seven days");
+                temp[i] = Convert.ToInt32(ReadLine());
+                total = temp[i] + total;
+            }
+            TempAverage = total / temp.Length;
+            WriteLine("Temperature average = " + TempAverage);
+            for (int j = 0; j < temp.Length; j++) {
+                WriteLine("The difference for temp " + temp[j] + " and the average is " + (temp[j] - TempAverage));
+            }
+            
+        }
+    }
+}

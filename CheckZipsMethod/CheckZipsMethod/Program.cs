07e46a0f68a44723cf6ac108c4da84ef2197a3ca
KyleBurton <kyleb286@gmail.com>
2018-12-13 13:18:58 -0600

    update

--- CheckZipsMethod/CheckZipsMethod/Program.cs
+++ CheckZipsMethod/CheckZipsMethod/Program.cs
@@ -5,15 +5,14 @@ using System.Text;
 using System.Threading.Tasks;
 using static System.Console;
 
-namespace CheckZips
+namespace CheckZipsMethod
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int[] zips = { 67777, 45555, 87654, 12345, 43527, 19476, 47385, 48676, 49337, 85633 };
+            int[] zips = { 11111, 22222, 33333, 44444, 55555, 66666, 77777, 88888, 99999, 10101 };
             int userZip = 0;
-            int result = 0;
 
             WriteLine("Enter a 5 digit zip code to see if you area code is eligible for us to deliver to you. >>> ");
             userZip = Convert.ToInt32(ReadLine());
@@ -23,13 +22,13 @@ namespace CheckZips
                 if (zips[i] == userZip)
                 {
                     WriteLine("We can deliver to your area!");
-                    result = 1;
                 }
                 else
                 {
                     WriteLine("We do not deliver to your area");
                 }
             }
-        }  
+           
+        }
     }
 }
